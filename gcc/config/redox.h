c72965bf986bb7361489d5657d1724e9a8df9a15
Jeremy Soller <jeremy@system76.com>
2019-01-07 17:36:55 -0700

    Always link pthreads

--- gcc/config/redox.h
+++ gcc/config/redox.h
@@ -2,7 +2,7 @@
 #define TARGET_REDOX 1
 
 #undef LIB_SPEC
-#define LIB_SPEC "%{pthread:-lpthread} -lc"
+#define LIB_SPEC "-lc -lpthread"
 
 #undef STARTFILE_SPEC
 #define STARTFILE_SPEC "crt0.o%s crti.o%s crtbegin.o%s"
