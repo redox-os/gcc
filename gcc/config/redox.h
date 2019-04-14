9e894ba09f7216d4591f243c3febcc32769e5720
Jeremy Soller <jeremy@system76.com>
2019-04-14 09:51:00 -0600

    Use crtbeginS.o and crtendS.o when compiling with pie

--- gcc/config/redox.h
+++ gcc/config/redox.h
@@ -8,10 +8,10 @@
 #define LINK_SPEC "%{shared:-shared} %{static:-static} %{!shared: %{!static: %{rdynamic:-export-dynamic}}}"
 
 #undef STARTFILE_SPEC
-#define STARTFILE_SPEC "%{!shared:crt0.o%s} crti.o%s %{shared:crtbeginS.o%s;:crtbegin.o%s}"
+#define STARTFILE_SPEC "%{!shared:crt0.o%s} crti.o%s %{shared|pie:crtbeginS.o%s;:crtbegin.o%s}"
 
 #undef ENDFILE_SPEC
-#define ENDFILE_SPEC   "%{shared:crtendS.o%s;:crtend.o%s} crtn.o%s"
+#define ENDFILE_SPEC   "%{shared|pie:crtendS.o%s;:crtend.o%s} crtn.o%s"
 
 #undef  NO_IMPLICIT_EXTERN_C
 #define NO_IMPLICIT_EXTERN_C 1
